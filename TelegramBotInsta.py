7018cd7be78518ee3fb4fbf31580068204a73bd2
throogoo <67383182+throogoo@users.noreply.github.com>
2020-12-05 23:03:47 +0900

    Update TelegramBotInsta.py

--- TelegramBotInsta.py
+++ TelegramBotInsta.py
@@ -13,7 +13,7 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-token = "YOUR_TOKEN_HERE"
+token = "1448637537:AAHpeskpUwok-49DTYJCvqB-xuoSEhEjfgk"
 
 
 
